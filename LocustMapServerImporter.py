a7a1cf8cfa902e656b1b76266f2d6904af5e4944
pl.marasco <pl.marasco@gmail.com>
2023-07-07 14:41:40 +0200

    Standardization, notes and minor issues

--- LocustMapServerImporter.py
+++ LocustMapServerImporter.py
@@ -14,115 +14,168 @@
 from datetime import datetime
 from osgeo import gdal, osr
 from MapServer import LayerInfo, MapServer
-from Utils import * 
-import numpy as np, os, sys, threading
+from Utils import *
+import os
+import sys
+
 gdal.UseExceptions()
 
 
 class LocustMapServerImporter(object):
-	def __init__(self, rootDir, url=""):
-		self._rootDir = rootDir
-		self._url = url
-		if self._url[-1] != "/":
-			self._url +="/"
-		
-	def _warpToEPSG(self, inFile, dstEPSG):
-		#warp if destination epsg is different than the file epsg
-		inDt = gdal.Open(inFile)
-		proj = osr.SpatialReference(wkt=inDt.GetProjection())
-		flEPSG = int(proj.GetAttrValue('AUTHORITY',1))
-		ret = inDt
-		
-		if(flEPSG != dstEPSG):
-			dstFile = os.path.join(datePath, str(dstEPSG) + "_mapserver_" + fl)
-			kwargs = {
-				"format":"GTiff",
-				"creationOptions": ["TILED=YES", "COMPRESS=LZW"],
-				"dstSRS": "EPSG:{0}".format(dstEPSG)
-			}
-			gdal.Warp(dstFile, inFile, **kwargs)
-			ret = gdal.Open(dstFile)
-			
-		return ret
-			
-		
-	def process(self, dstEPSG=4326):
-		errorHandler = GDALErrorHandler()
-		gdal.PushErrorHandler(errorHandler.handler)
-		#get regions
-		regions = [ f for f in os.listdir(self._rootDir) if os.path.isdir(os.path.join(self._rootDir, f))] 
-		for region in regions:
-			regionPath = os.path.join(self._rootDir, region)
-		
-			layerList = []
-			
-			#for each date....
-			for date in os.listdir(regionPath):
-				if date == "archive" or not os.path.isdir(os.path.join(regionPath,date)):
-					continue
-				
-				#get all gdal-compliant files from folder
-				datePath = os.path.join(regionPath, date)
-				for fl in os.listdir(datePath):
-					if os.path.isdir(fl) or fl.endswith("ovr") or "RGB" not in fl:
-						continue
-					inFile = os.path.join(datePath, fl)
+    def __init__(self, rootDir: str, url: str = ""):
+        """
+        rootDir: root directory of the locust data
+        url: url of the mapserver
+        """
+        self._rootDir = rootDir
+        self._url = url
+        if self._url[-1] != "/":
+            self._url += "/"
 
-					try:
-						inDt = self. _warpToEPSG(inFile, dstEPSG)
-							
-						#build pyramids
-						overview = inFile + ".ovr"
-						#validate overviews
-						if os.path.isfile(overview):
-							try:
-								overviewDt = gdal.Open(overview)
-								errorHandler.capture()
-							except Exception as e:
-								os.remove(overview)
+    def _warpToEPSG(
+        self, inFile: str, dstEPSG: int, datePath: str, fl: str
+    ) -> gdal.Dataset:
+        """
+        Warp if destination EPGS code is different from the file's EPSG
 
-						if not os.path.isfile(overview):
-							print ("Building pyramids for: ", inDt.GetDescription())
-							gdal.SetConfigOption('COMPRESS_OVERVIEW', 'DEFLATE')
-							inDt.BuildOverviews(resampling="AVERAGE", overviewlist=[2, 4, 8, 16, 32, 64])
-						
-						
-						#append to layerlist
-						layerList.append(LayerInfo(os.path.relpath(inFile, regionPath), 
-							os.path.split(inFile)[-1].split(".")[0],
-							"EPSG:{0}".format(dstEPSG), 
-							inDt.RasterXSize, 
-							inDt.RasterYSize,
-							getGDALRasterExtents(inDt), datetime.strptime(date, "%Y%m%d").isoformat()[0:10] ))
+        inFile: input file
+        dstEPSG: destination epsg
 
-						
-					except Exception as e:
-						print("Error: ", e)
-			
-			#sort files....
-			layerList.sort(key=lambda x: x.date, reverse=True)
-			latestDate = layerList[0].date
-			stop = False
-			i = 0
-			while not stop:
-				if layerList[i].date == latestDate:
-					layerList[i].layerName +="_LATEST"
-					i += 1
-				else:
-					stop = True
-			
-			#run mapserver file generator
-			obj = MapServer(layerList, self._url+region, os.path.join(regionPath, "mapserver.map"), "Locust WMS Service")
-			obj.process()
+        Return gdal dataset
+        """
+
+        try:
+            inDt = gdal.Open(inFile)
+        except Exception as e:
+            print("Error opening the file: ", inFile)
+            return None
+
+        proj = osr.SpatialReference(wkt=inDt.GetProjection())
+        flEPSG = int(proj.GetAttrValue("AUTHORITY", 1))
+
+        if flEPSG != dstEPSG:
+            dstFile = os.path.join(datePath, str(dstEPSG) + "_mapserver_" + fl)
+            kwargs = {
+                "format": "GTiff",
+                "creationOptions": ["TILED=YES", "COMPRESS=LZW"],
+                "dstSRS": "EPSG:{0}".format(dstEPSG),
+            }
+            gdal.Warp(dstFile, inFile, **kwargs)
+            ret = gdal.Open(dstFile)
+        else:
+            ret = inDt
+
+        return ret
+
+    def process(self, dstEPSG: int = 4326) -> None:
+        """
+        Process the data and upload them to the mapserver
+        :param dstEPSG: int, destination EPSG code
+        :return: None
+        """
+        # set error handler
+        errorHandler = GDALErrorHandler()
+        gdal.PushErrorHandler(errorHandler.handler)
+        # get regions
+        regions = [
+            f
+            for f in os.listdir(self._rootDir)
+            if os.path.isdir(os.path.join(self._rootDir, f))
+        ]
+        for region in regions:
+            regionPath = os.path.join(self._rootDir, region)
+
+            layerList = []
+
+            # for each date....
+            for date in os.listdir(regionPath):
+                if date == "archive" or not os.path.isdir(
+                    os.path.join(regionPath, date)
+                ):
+                    continue
+
+                # get all gdal-compliant files from folder
+                datePath = os.path.join(regionPath, date)
+                for fl in os.listdir(datePath):
+                    if os.path.isdir(fl) or fl.endswith("ovr") or "RGB" not in fl:
+                        continue
+                    inFile = os.path.join(datePath, fl)
+
+                    try:
+                        inDt = self._warpToEPSG(
+                            inFile, dstEPSG
+                        )  # TODO add datepath and fl
+
+                        # build pyramids
+                        overview = inFile + ".ovr"
+                        # validate overviews
+                        if os.path.isfile(overview):
+                            try:
+                                overviewDt = gdal.Open(
+                                    overview
+                                )  # TODO overviewDt seems to be unused
+                                errorHandler.capture()
+                            except Exception as e:
+                                os.remove(overview)
+
+                        if not os.path.isfile(overview):
+                            print("Building pyramids for: ", inDt.GetDescription())
+                            gdal.SetConfigOption("COMPRESS_OVERVIEW", "DEFLATE")
+                            inDt.BuildOverviews(
+                                resampling="AVERAGE", overviewlist=[2, 4, 8, 16, 32, 64]
+                            )
+
+                        # append to layerlist
+                        layerList.append(
+                            LayerInfo(
+                                os.path.relpath(inFile, regionPath),
+                                os.path.split(inFile)[-1].split(".")[0],
+                                "EPSG:{0}".format(dstEPSG),
+                                inDt.RasterXSize,
+                                inDt.RasterYSize,
+                                getGDALRasterExtents(inDt),
+                                datetime.strptime(date, "%Y%m%d").isoformat()[0:10],
+                            )
+                        )
+
+                    except Exception as e:
+                        print("Error: ", e)
+
+            # sort files....
+            layerList.sort(key=lambda x: x.date, reverse=True)
+            latestDate = layerList[0].date
+            stop = False
+            i = 0
+            while not stop:
+                if layerList[i].date == latestDate:
+                    layerList[i].layerName += "_LATEST"
+                    i += 1
+                else:
+                    stop = True
+
+            # run mapserver file generator
+            obj = MapServer(
+                layerList,
+                self._url + region,
+                os.path.join(regionPath, "mapserver.map"),
+                "Locust WMS Service",
+            )
+            obj.process()
+
 
 def main():
-	if len(sys.argv) < 3:
-		print("usage: python LocustMapServerImporter.py root_dir root_wms_url")
-		return 1
-	obj = LocustMapServerImporter(sys.argv[1], sys.argv[2])
-	obj.process()	
+    """
+    Main function
+    :return: None
+    """
 
+    assert (
+        len(sys.argv) < 3
+    ), "usage: python LocustMapServerImporter.py root_dir root_wms_url"
+
+    obj = LocustMapServerImporter(sys.argv[1], sys.argv[2])
+    obj.process()
 
 
 if __name__ == "__main__":
-	main()
+    main()
